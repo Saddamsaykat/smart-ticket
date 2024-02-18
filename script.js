bf018f84f109d1e55f5c461edaf928c60b46a4d2
Saddamsaykat <mdshaikat965@gmail.com>
2024-02-19 01:07:44 +0600

    ui update one

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const busSets = document.getElementsByClassName("btn-sets")
-for (const busSet of busSets) {
-    busSet.addEventListener("click", function (event) {
-        busSet.style.backgroundColor = "green";
-        busSet.style.fontSize = "20px";
-        busSet.style.color = "White"
-        console.log(busSet.innerText);
+// const busSets = document.getElementsByClassName("btn-sets")
+// for (const busSet of busSets) {
+//     busSet.addEventListener("click", function (event) {
+//         busSet.style.backgroundColor = "green";
+//         busSet.style.fontSize = "20px";
+//         busSet.style.color = "White"
+//         allSetsBuy.appendChild(event.innerText);
 
 
-    })
-}
+//         const economy = document.createElement("p");
+//         economy.innerText = "Economy";
+//         const price = document.createElement("p");
+//         price.innerText = "550";
+//         const allSetsBuy = document.getElementById("all-sets-buy");
+//         allSetsBuy.appendChild(economy);
+//         allSetsBuy.appendChild(price);
 
-function btnSetsAll(eventFncVal) {
-    const dataSet = eventFncVal.innerText
-    console.log(dataSet);
+//     })
+// }
+
+function button(event) {
+    console.log("Hello");
 }
