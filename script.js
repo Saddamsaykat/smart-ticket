29fae582ee8d606ecdc04f68c4d9a576faa78239
Saddamsaykat <mdshaikat965@gmail.com>
2024-02-19 18:58:35 +0600

    functional

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let numOfClick = 0;
 
+
 function button(event) {
 
     numOfClick = numOfClick + 1;
+    const dec = document.getElementById("total-sets");
+    const sumSets = parseInt(dec.innerText) - 1;
+    dec.innerText = sumSets;
+
+
 
     const seatCount = document.getElementById("seat-count");
     seatCount.innerText = numOfClick;
 
     const clickButton = event.target;
     const buttonText = clickButton.innerText;
-    clickButton.style.backgroundColor = "green";
-    clickButton.style.fontSize = "20px";
-    clickButton.style.color = "white";
+    // button color change
+
+
+
     const classSets = document.createElement("p");
     classSets.innerText = "Economy";
 
@@ -20,7 +27,13 @@ function button(event) {
 
     const buttonTextParagraph = document.createElement("p");
     buttonTextParagraph.innerText = buttonText;
-
+    if (numOfClick > 4) {
+        alert("One Person Can Only buying 4 Tickets (Only)")
+        return
+    }
+    clickButton.style.backgroundColor = "green";
+    clickButton.style.fontSize = "20px";
+    clickButton.style.color = "white";
     const setsBuy = document.getElementById("all-sets-buy");
     setsBuy.appendChild(buttonTextParagraph);
     setsBuy.appendChild(classSets);
@@ -32,4 +45,49 @@ function button(event) {
     const ticketPrizeBDT = document.getElementById("total-prize");
     ticketPrizeBDT.innerText = sumValue;
 
+    const grandPrize = document.getElementById("grand-total");
+    grandPrize.innerText = sumValue;
+
+    event.target.setAttribute("disabled", true);
 }
+
+// cupon
+
+document.getElementById("cupon-btn").addEventListener("click", function lessValue() {
+    const cuponCode = document.getElementById("text-area").value;
+    if (cuponCode == "new15") {
+        const totalGrand = document.getElementById("grand-total")
+        const baseValue = parseInt(totalGrand.innerText);
+        const discount = baseValue * 0.15;
+        const discountValue = baseValue - discount;
+        totalGrand.innerText = discountValue;
+        console.log(discountValue);
+    } else if (cuponCode == "Couple 20") {
+        const totalGrand = document.getElementById("grand-total")
+        const baseValue = parseInt(totalGrand.innerText);
+        const discount = baseValue * 0.15;
+        const discountValue = baseValue - discount;
+        totalGrand.innerText = discountValue;
+        console.log(discountValue);
+    }
+})
+
+// modal
+const nextBtn = document.getElementById("next-btn");
+const passengerInput = document.getElementById("passenger");
+const phoneInput = document.getElementById("phone")
+
+function enableButton() {
+    if (passengerInput.value !== "" && phoneInput.value !== "") {
+        nextBtn.removeAttribute("disabled");
+    } else {
+        nextBtn.removeAttribute("disabled", true);
+    }
+}
+passengerInput.addEventListener("input", enableButton)
+phoneInput.addEventListener("input", enableButton)
+
+
+document.getElementById("btn-close").addEventListener("click", function () {
+    document.getElementById("my_modal_5").style.display = "none";
+})
